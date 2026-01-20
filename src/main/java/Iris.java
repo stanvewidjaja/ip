4582f3b2b1bd3f291c474d02dc272bf0325c518d
stanvewidjaja <90084856+stanvewidjaja@users.noreply.github.com>
2026-01-20 22:34:52 +0800

    Level-0: rename Duke to Iris, add greeting

---
+++ src/main/java/Iris.java
@@ -0,0 +1,14 @@
+public class Iris {
+    public static void main(String[] args) {
+        String message = "";
+        String LINE = "________________________" +
+                "____________________________________\n";
+        message += LINE;
+        message += "Hello! I'm Iris!\n";
+        message += "What can I do for you?\n";
+        message += LINE;
+        message += "Bye. Hope to see you again soon!\n";
+        message += LINE;
+        System.out.println(message);
+    }
+}
