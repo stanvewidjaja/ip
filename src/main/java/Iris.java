e1a91b0a5d0129e2be30f5d8866dd3533b76973b
stanvewidjaja <90084856+stanvewidjaja@users.noreply.github.com>
2026-01-20 23:01:04 +0800

    add looping functionality, Level-1 complete

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Iris {
     public static void main(String[] args) {
         String message = "";
@@ -7,8 +9,19 @@ public class Iris {
         message += "Hello! I'm Iris!\n";
         message += "What can I do for you?\n";
         message += LINE;
-        message += "Bye. Hope to see you again soon!\n";
-        message += LINE;
+
         System.out.println(message);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String inp = sc.nextLine();
+            if (inp.equals("bye")) {
+                break;
+            }
+            System.out.println(LINE + inp + "\n" + LINE);
+        }
+        String exit_msg = "Bye. Hope to see you again soon!\n"
+        + LINE;
+        System.out.println(exit_msg);
     }
 }
