3d6d06f20f1f1f336a7ac11f93bd919b7dad9cbc
stanvewidjaja <90084856+stanvewidjaja@users.noreply.github.com>
2026-01-21 14:37:40 +0800

    Added Level-2: add and list

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -10,18 +10,30 @@ public class Iris {
         message += "What can I do for you?\n";
         message += LINE;
 
-        System.out.println(message);
+        System.out.print(message);
 
         Scanner sc = new Scanner(System.in);
+
+        String[] taskList = new String[100];
+        int index = 0;
         while (true) {
             String inp = sc.nextLine();
             if (inp.equals("bye")) {
                 break;
+            } else if (inp.equals("list")) {
+                System.out.print(LINE);
+                for (int i = 0; i < index; i++) {
+                    System.out.println((i + 1) + ". " + taskList[i]);
+                }
+                System.out.print(LINE);
+            } else {
+                taskList[index] = inp;
+                index += 1;
+                System.out.print(LINE + "added: " + inp + "\n" + LINE);
             }
-            System.out.println(LINE + inp + "\n" + LINE);
         }
-        String exit_msg = "Bye. Hope to see you again soon!\n"
+        String exitMsg = "Bye. Hope to see you again soon!\n"
         + LINE;
-        System.out.println(exit_msg);
+        System.out.print(exitMsg);
     }
 }
