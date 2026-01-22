049c37cc2468b6b49b58ca3d92730a6b7aa45b86
stanvewidjaja <90084856+stanvewidjaja@users.noreply.github.com>
2026-01-22 08:00:54 +0800

    Add level 5 skeleton of IrisException and refactor input processing

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -31,6 +31,39 @@ public class Iris {
         return ret;
     }
 
+    private static int processInput(String inp, Task[] taskList, int index) throws IrisException {
+        String[] parts = inp.split(" ");
+        if (inp.equals("list")) {
+            String listMsg = "Your tasks, printed:\n";
+            for (int i = 0; i < index; i++) {
+                listMsg += (i + 1) + "." + taskList[i].toString() + "\n";
+            }
+            printBox(listMsg);
+        } else if (parts[0].equals("mark")) {
+            String markMsg = "You have done the task. Good job!\n";
+            int taskNum = Integer.parseInt(parts[1]);
+            Task task = taskList[taskNum - 1];
+            task.markDone();
+            markMsg += "  " + task + "\n";
+            printBox(markMsg);
+        } else if (parts[0].equals("unmark")) {
+            String unmarkMsg = "OK, I have marked it as not done.\n";
+            int taskNum = Integer.parseInt(parts[1]);
+            Task task = taskList[taskNum - 1];
+            task.markUndone();
+            unmarkMsg += "  " + task + "\n";
+            printBox(unmarkMsg);
+        } else {
+            Task newTask = processTask(inp);
+            taskList[index] = newTask;
+            index += 1;
+            String addMessage = "Okay. I've added this task:\n  " + newTask
+                    + "\nNow you have " + index + " tasks in the list.\n";
+            printBox(addMessage);
+        }
+        return index;
+    }
+
     public static void main(String[] args) {
         String greetMsg =
                 "Hello! I'm Iris!\n"
@@ -43,36 +76,13 @@ public class Iris {
         int index = 0;
         while (true) {
             String inp = sc.nextLine();
-            String[] parts = inp.split(" ");
             if (inp.equals("bye")) {
                 break;
-            } else if (inp.equals("list")) {
-                String listMsg = "Your tasks, printed:\n";
-                for (int i = 0; i < index; i++) {
-                    listMsg += (i + 1) + "." + taskList[i].toString() + "\n";
-                }
-                printBox(listMsg);
-            } else if (parts[0].equals("mark")) {
-                String markMsg = "You have done the task. Good job!\n";
-                int taskNum = Integer.parseInt(parts[1]);
-                Task task = taskList[taskNum - 1];
-                task.markDone();
-                markMsg += "  " + task + "\n";
-                printBox(markMsg);
-            } else if (parts[0].equals("unmark")) {
-                String unmarkMsg = "OK, I have marked it as not done.\n";
-                int taskNum = Integer.parseInt(parts[1]);
-                Task task = taskList[taskNum - 1];
-                task.markUndone();
-                unmarkMsg += "  " + task + "\n";
-                printBox(unmarkMsg);
-            } else {
-                Task newTask = processTask(inp);
-                taskList[index] = newTask;
-                index += 1;
-                String addMessage = "Okay. I've added this task:\n  " + newTask
-                        + "\nNow you have " + index + " tasks in the list.\n";
-                printBox(addMessage);
+            }
+            try {
+                index = processInput(inp, taskList, index);
+            } catch (IrisException ie) {
+                printBox(ie.getMessage() + "\n");
             }
         }
         String exitMsg = "Bye. Hope to see you again soon!\n"
---
+++ src/main/java/IrisException.java
@@ -0,0 +1,5 @@
+public class IrisException extends Exception {
+    public IrisException(String errorMsg) {
+        super(errorMsg);
+    }
+}
