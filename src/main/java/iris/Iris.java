5328b1ffff444680e406ef45f9019778640d4988
stanvewidjaja <90084856+stanvewidjaja@users.noreply.github.com>
2026-02-16 20:11:51 +0800

    Refactor Iris and Parser

--- src/main/java/iris/Iris.java
+++ src/main/java/iris/Iris.java
@@ -10,42 +10,63 @@ import java.util.Scanner;
 public class Iris {
     private TaskList taskList;
 
+    private static String handleMarkLike(String inp, TaskList taskList, String markOrUnmark) throws IrisException {
+        int index = Parser.parseIndexOrThrow(inp, taskList);
+        Task task = taskList.get(index);
+        boolean isMark = markOrUnmark.equals("mark");
+
+        if (isMark) {
+            task.markDone();
+            Storage.saveTasksToFile(taskList);
+            return Ui.renderMarkTask(task);
+        } else {
+            task.markUndone();
+            Storage.saveTasksToFile(taskList);
+            return Ui.renderUnmarkTask(task);
+        }
+    }
+
+    private static String handleDelete(String inp, TaskList taskList) throws IrisException {
+        int index = Parser.parseIndexOrThrow(inp, taskList);
+        Task task = taskList.remove(index);
+        Storage.saveTasksToFile(taskList);
+        return Ui.renderDeleteTask(task, taskList);
+    }
+
+    private static String handleFind(String inp, TaskList taskList) throws IrisException {
+        String keyword = Parser.parseFindKeywordOrThrow(inp);
+        TaskList found = taskList.findByKeyword(keyword);
+        return Ui.renderFoundTasksList(found);
+    }
+
+    private static String handleCreateTask(String inp, TaskList taskList) throws IrisException {
+        Task newTask = Parser.processTask(inp);
+        taskList.add(newTask);
+        Storage.saveTasksToFile(taskList);
+        return Ui.renderNewTask(newTask, taskList);
+    }
+
     private static String processInput(String inp, TaskList taskList) throws IrisException {
         String command = Parser.getCommand(inp);
+        if (command.isEmpty()) {
+            throw new IrisException("Please enter a command.");
+        }
+
         boolean isTaskCommand = Parser.isTaskCreationCommand(command);
+        boolean isMarkLikeCommand = Parser.isMarkLikeCommand(command);
 
         if (command.equals("bye")) {
             return Ui.renderExitMsg();
         } else if (command.equals("list")) {
             return Ui.renderTaskList(taskList);
         } else if (command.equals("find")) {
-            String keyword = Parser.parseFindKeywordOrThrow(inp);
-            TaskList found = taskList.findByKeyword(keyword);
-            return Ui.renderFoundTasksList(found);
+            return handleFind(inp, taskList);
         } else if (command.equals("delete")) {
-            int index = Parser.parseIndexOrThrow(inp, taskList);
-            Task task = taskList.remove(index);
-            Storage.saveTasksToFile(taskList);
-            return Ui.renderDeleteTask(task, taskList);
-        } else if (command.equals("mark")) {
-            int index = Parser.parseIndexOrThrow(inp, taskList);
-            Task task = taskList.get(index);
-            task.markDone();
-            Storage.saveTasksToFile(taskList);
-            return Ui.renderMarkTask(task);
-        } else if (command.equals("unmark")) {
-            int index = Parser.parseIndexOrThrow(inp, taskList);
-            Task task = taskList.get(index);
-            task.markUndone();
-            Storage.saveTasksToFile(taskList);
-            return Ui.renderUnmarkTask(task);
+            return handleDelete(inp, taskList);
+        } else if (isMarkLikeCommand) {
+            return handleMarkLike(inp, taskList, command);
         } else if (isTaskCommand) {
-            Task newTask = Parser.processTask(inp);
-            taskList.add(newTask);
-            Storage.saveTasksToFile(taskList);
-            return Ui.renderNewTask(newTask, taskList);
-        } else if (command.isEmpty()) {
-            throw new IrisException("Please enter a command.");
+            return handleCreateTask(inp, taskList);
         } else {
             throw new IrisException("I don't recognize that command.");
         }
